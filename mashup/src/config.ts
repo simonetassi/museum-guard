537e13fd741c187103f3cd6e3f32194895c2d2fc
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-05-29 16:52:59 +0200

    feat(mashup-sensor): add influx variables to config

--- mashup/src/config.ts
+++ mashup/src/config.ts
@@ -4,4 +4,9 @@ export const CONFIG = {
   sensor: {
     tdUrl: process.env.CONTROLLER_HTTP_PORT ?? 'http://localhost:8080/museumguard-sensor'
   },
+  influxdb: {
+    url: process.env.INFLUX_URL ?? "http://localhost:8181",
+    token: process.env.INFLUX_TOKEN ?? "token",
+    database: process.env.INFLUX_DATABASE ?? "museumguard",
+  },
 } as const;
